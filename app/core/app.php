e1b6c51788a6d15427f87726933b5bffdc664d44
HardCoreWare <bernardocodu@gmail.com>
2019-06-10 00:46:04 -0500

    commenting

--- app/core/app.php
+++ app/core/app.php
@@ -5,8 +5,9 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-
-
+/************************/
+/*****SLIM-INSTANCE******/
+/************************/
 $app = new \Slim\App([
 
     'settings' => [
